ac60e0bd11597b462c8820de2ddb75d22e3da5fb
Carlos Abdalla <abdalla@msdev.com.br>
2019-02-25 19:04:06 -0300

    new version

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var hotjar = require('./src/react-hotjar');
 
 module.exports = {
 	hotjar: {
-		initialize: function initialize(id, sv) {
+		initialize: (initialize = (id, sv) => {
 			hotjar(id, sv);
-		}
+		})
 	}
 };
