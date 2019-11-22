1e1a79d52d39f4ff69bb55c49c6ad17e20fe8c89
Elyjohn <elyjohn.zucca@gmail.com>
2019-11-22 16:31:22 +0100

    Add typings (#13)
    Add typescript typings for the main function.

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Implement Hotjar into your react application.
+ * @param hjid This is the ID which tells Hotjar which site settings it should load and where it should save the data collected.
+ * @param hjsv The version of the Tracking Code you are using. This is only needed if Hotjar ever updates the Tracking Code and needs to discontinue older ones. Knowing which version your site includes allows hotjar team to contact you and inform you accordingly.
+ */
+export function hotjar(hjid: number, hjsv: number): void;
