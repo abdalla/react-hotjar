96ae4112318d153a2ed69e10785ce758bee71a06
Elyjohn <ely.zucca@richemont.com>
2019-11-25 11:39:00 +0100

    Fix typings
    Fix typings by making hotjar as module and add initialize function to it

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
-/**
- * Implement Hotjar into your react application.
- * @param hjid This is the ID which tells Hotjar which site settings it should load and where it should save the data collected.
- * @param hjsv The version of the Tracking Code you are using. This is only needed if Hotjar ever updates the Tracking Code and needs to discontinue older ones. Knowing which version your site includes allows hotjar team to contact you and inform you accordingly.
- */
-export function hotjar(hjid: number, hjsv: number): void;
+export module hotjar {
+  /**
+   * Implement Hotjar into your react application.
+   * @param hjid This is the ID which tells Hotjar which site settings it should load and where it should save the data collected.
+   * @param hjsv The version of the Tracking Code you are using. This is only needed if Hotjar ever updates the Tracking Code and needs to discontinue older ones. Knowing which version your site includes allows hotjar team to contact you and inform you accordingly.
+   */
+  export function initialize(hjid: number, hjsv: number): void;
+}
