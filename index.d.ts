e1caf3c7d9ebea1524a74e2a274fc086676c7792
Matas Seimys <mseimys@users.noreply.github.com>
2021-07-01 22:10:53 +0300

    Add Identify typings (#31)

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,18 @@
 export module hotjar {
   /**
-   * Implement Hotjar into your react application.
+   * Initialize Hotjar tracking.
    * @param hjid This is the ID which tells Hotjar which site settings it should load and where it should save the data collected.
    * @param hjsv The version of the Tracking Code you are using. This is only needed if Hotjar ever updates the Tracking Code and needs to discontinue older ones. Knowing which version your site includes allows hotjar team to contact you and inform you accordingly.
    */
   export function initialize(hjid: number, hjsv: number): void;
+
+  /**
+   * Identify user
+   * @param userId Unique ID of a user
+   * @param properties Additional properties describing your user
+   */
+  export function identify(
+    userId: string,
+    properties: Record<string, any>
+  ): void;
 }
