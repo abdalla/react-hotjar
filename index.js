ed007eac036053fc16225b1e9e454aead608f7d3
James Starkie <12033907+jstarmx@users.noreply.github.com>
2022-08-25 16:31:10 +0100

    Add helper function to check if Hotjar has been intialized (#37)

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ function hj() {
 		throw new Error('Hotjar is not initialized');
 	}
 
-	window.hj.apply(undefined, params)
+	window.hj.apply(undefined, params);
 }
 
 module.exports = {
 	hotjar: {
 		initialize: function initialize(id, sv) {
 			hotjar(id, sv);
+		},
+		initialized: function initialized() {
+			return typeof window.hj === 'function';
 		},
 		identify: function identify(userId, properties) {
 			hj('identify', userId, properties);
