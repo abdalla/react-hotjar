8b4605edf8f0d490077489fa1701a69f2c3bf7c0
Carlos Abdalla <abdalla@users.noreply.github.com>
2023-01-13 09:12:24 -0300

    added debug parameter (#49)
    Co-authored-by: Carlos Abdalla <carlos.abdalla@ifood.com.br>

--- index.d.ts
+++ index.d.ts
@@ -3,8 +3,9 @@ export module hotjar {
    * Initialize Hotjar tracking.
    * @param hjid This is the ID which tells Hotjar which site settings it should load and where it should save the data collected.
    * @param hjsv The version of the Tracking Code you are using. This is only needed if Hotjar ever updates the Tracking Code and needs to discontinue older ones. Knowing which version your site includes allows hotjar team to contact you and inform you accordingly.
+   * @param debug [debug=false] Used to enable debug mode
    */
-  export function initialize(hjid: number, hjsv: number): void;
+  export function initialize(hjid: number, hjsv: number, debug: boolean): void;
 
   /**
    * Check if Hotjar has been initialized
