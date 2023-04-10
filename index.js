f0f183abdb56c45bfb6cc1e4c004caa46e890653
abdalla <abdalla@msdev.com.br>
2023-04-10 17:54:17 -0300

    small fixes

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-var hotjar = require('./src/react-hotjar');
+import hotjar from './src/react-hotjar';
 
 function hj() {
-	var params = Array.prototype.slice.call(arguments);
+	const params = Array.prototype.slice.call(arguments);
 	if (!window.hj) {
 		throw new Error('Hotjar is not initialized');
 	}
-
 	window.hj.apply(undefined, params);
 }
 
-module.exports = {
-	hotjar: {
-		initialize: function initialize(id, sv) {
-			hotjar(id, sv);
-		},
-		initialized: function initialized() {
-			return typeof window !== 'undefined' && typeof window?.hj === 'function';
-		},
-		identify: function identify(userId, properties) {
-			hj('identify', userId, properties);
-		},
-		event: function event(event) {
-			hj('event', event);
-		},
-		stateChange: function stateChange(relativePath) {
-			hj('stateChange', relativePath);
-		}
-	}
+export const hotjar = {
+	initialize: function initialize(id, sv) {
+		hotjar(id, sv);
+	},
+	initialized: function initialized() {
+		return typeof window !== 'undefined' && typeof window.hj === 'function';
+	},
+	identify: function identify(userId, properties) {
+		hj('identify', userId, properties);
+	},
+	event: function event(event) {
+		hj('event', event);
+	},
+	stateChange: function stateChange(relativePath) {
+		hj('stateChange', relativePath);
+	},
 };
